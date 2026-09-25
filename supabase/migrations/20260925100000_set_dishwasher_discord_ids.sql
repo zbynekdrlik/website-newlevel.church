09cbf1efca23b2c4be0ea86de12b5dea173dbb0f
Kristian <kukos700@gmail.com>
2026-09-25 09:21:00 +0200

    data: add dishwasher Discord user ids

---
+++ supabase/migrations/20260925100000_set_dishwasher_discord_ids.sql
@@ -0,0 +1,15 @@
+update invitation.dishwasher_members
+set discord_user_id = '1123704065554124840'
+where id = 'da100000-0000-4000-8000-000000000002'; -- Judit
+
+update invitation.dishwasher_members
+set discord_user_id = '1123686510613512243'
+where id = 'da100000-0000-4000-8000-000000000010'; -- Patrika
+
+update invitation.dishwasher_members
+set discord_user_id = '945727006203580487'
+where id = 'da100000-0000-4000-8000-000000000009'; -- Katka
+
+update invitation.dishwasher_members
+set discord_user_id = '1125353373139468289'
+where id = 'da100000-0000-4000-8000-000000000003'; -- Gabika
